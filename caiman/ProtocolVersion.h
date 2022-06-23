0d68d0de0c5ff3136dd3163dea3167d4ff814914
Ben Gainey <ben.gainey@arm.com>
2022-06-23 09:43:21 +0100

    Caiman 8.0.0

--- caiman/ProtocolVersion.h
+++ caiman/ProtocolVersion.h
@@ -1,10 +1,10 @@
-/* Copyright (C) 2021 by Arm Limited. All rights reserved. */
+/* Copyright (C) 2022 by Arm Limited. All rights reserved. */
 #pragma once
 
 /* Define the product release version / protocol version */
 
-// Protocol version Streamline v7.9
-#define CAIMAN_VERSION 790
+// Protocol version Streamline v8.0
+#define CAIMAN_VERSION 800
 // Differentiates development versions from release code
 #define CAIMAN_VERSION_DEV_MULTIPLIER 100000
 
