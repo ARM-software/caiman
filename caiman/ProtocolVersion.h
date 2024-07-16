087f73713fc7201fae8a846cede22a8f1e108322
Ben Gainey <ben.gainey@arm.com>
2024-07-16 09:32:32 +0100

    Caiman 9.1.0

--- caiman/ProtocolVersion.h
+++ caiman/ProtocolVersion.h
@@ -3,8 +3,8 @@
 
 /* Define the product release version / protocol version */
 
-// Protocol version Streamline v9.0
-#define CAIMAN_VERSION 900
+// Protocol version Streamline v9.1
+#define CAIMAN_VERSION 910
 // Differentiates development versions from release code
 #define CAIMAN_VERSION_DEV_MULTIPLIER 100000
 
